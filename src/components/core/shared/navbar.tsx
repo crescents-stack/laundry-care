fe5bb5eb96adb845511ae1acab45a006a4bfdf1b
musiur <musiur.opu@gmail.com>
2023-09-22 02:48:38 +0600

    [Feature] Under maintenance banner #26

--- src/components/core/shared/navbar.tsx
+++ src/components/core/shared/navbar.tsx
@@ -32,9 +32,14 @@ const Navbar = () => {
   return (
     <nav
       className={`sticky top-0 z-50 transition ease-in-out duration-500  ${
-        !navStyle ? "bg-[hsl(var(--primary-600))] shadow" : "bg-lighter-50 shadow-lg"
+        !navStyle
+          ? "bg-[hsl(var(--primary-600))] shadow"
+          : "bg-lighter-50 shadow-lg"
       }`}
     >
+      <div className="py-1 text-center bg-[hsl(var(--primary-900))] text-white font-bold  animate-pulse">
+      Under Maintenance
+      </div>
       <div
         className={`flex items-center justify-between gap-10 px-4 md:px-10 container py-2 md:py-4`}
       >
@@ -45,7 +50,9 @@ const Navbar = () => {
           }`}
         >
           <Logo
-            color={`${!navStyle ? "fill-yellow-300" : "fill-[hsl(var(--primary-500))]"}`}
+            color={`${
+              !navStyle ? "fill-yellow-300" : "fill-[hsl(var(--primary-500))]"
+            }`}
             height="50"
             width="50"
           />
@@ -72,20 +79,22 @@ const Navbar = () => {
             pathname={pathname}
           />
           <div className="flex items-center gap-1">
-          <ModeToggle />
-          <Link
-            href={`${pathname.includes("/bn") ? "/bn" : "/en"}/auth/user/login`}
-          >
-            <Button
-              className={`${
-                navStyle
-                  ? "text-white bg-[hsl(var(--primary-600))]"
-                  : "text-[hsl(var(--primary-600))] bg-lighter-50 hover:bg-lighter-200"
-              }`}
+            <ModeToggle />
+            <Link
+              href={`${
+                pathname.includes("/bn") ? "/bn" : "/en"
+              }/auth/user/login`}
             >
-              {dictionary.navbar.login}
-            </Button>
-          </Link>
+              <Button
+                className={`${
+                  navStyle
+                    ? "text-white bg-[hsl(var(--primary-600))]"
+                    : "text-[hsl(var(--primary-600))] bg-lighter-50 hover:bg-lighter-200"
+                }`}
+              >
+                {dictionary.navbar.login}
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
