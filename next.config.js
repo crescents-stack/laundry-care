9b92221bc6ea3613dc002034076875a166566505
Musiur Alam Opu <musiur.opu@gmail.com>
2023-09-25 01:02:22 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BACKEND_URL: "http://localhost:8000/api",
-        // BACKEND_URL: "https://laundry-care-backend.vercel.app/"
+        // BACKEND_URL: "http://localhost:8000/api",
+        BACKEND_URL: "https://laundry-care-backend.vercel.app/"
     }
 }
 
