58766f8be6836078450772e7bb2f8bfb0b264013
musiur <musiur.opu@gmail.com>
2023-09-25 00:33:39 +0600

    [Config] Backend Base URL added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BACKEND_URL: "http://localhost:8000/api"
+        BACKEND_URL_LOCAL: "http://localhost:8000/api",
+        BACKEND_URL_PRODU: "https://laundry-care-backend.vercel.app/"
     }
 }
 
