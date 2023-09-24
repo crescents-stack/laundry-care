704d9f1559b6620d07cbd24207390053de376344
musiur <musiur.opu@gmail.com>
2023-09-25 00:35:56 +0600

    [Config] OPU branch create From PROD

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // BACKEND_URL_LOCAL: "http://localhost:8000/api",
-        BACKEND_URL_PRODU: "https://laundry-care-backend.vercel.app/"
+        BACKEND_URL_LOCAL: "http://localhost:8000/api",
+        // BACKEND_URL_PRODU: "https://laundry-care-backend.vercel.app/"
     }
 }
 
