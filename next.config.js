d1faa0b0f937fd068f1a231f22f97a1011aefcc2
musiur <musiur.opu@gmail.com>
2023-10-01 16:14:26 +0600

    backend url update

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        BACKEND_URL: "http://localhost:8000/api",
+        // BACKEND_URL: "https://laundry-care-backend.vercel.app/api"
+    }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
+
