e8bd7317b17d313895280eda7f2b7e3a0a62bf27
musiur <musiur.opu@gmail.com>
2023-10-03 09:56:21 +0600

    [Feature] User > Admin copying functionalities #71

--- src/app/[lang]/admin/auth/forget-password/page.tsx
+++ src/app/[lang]/admin/auth/forget-password/page.tsx
@@ -1,31 +1,60 @@
 "use client";
 
 import ErrorMessage from "@/components/core/shared/error-message";
-import PhoneNumberInput from "@/components/core/shared/phone-input";
 import { H3 } from "@/components/core/typegraphy/headings";
 import { Button } from "@/components/ui/button";
+import { ButtonLoading } from "@/components/ui/button-loading";
+import { toast } from "@/components/ui/use-toast";
+import axios from "axios";
 import { ChangeEvent, MouseEvent, useState } from "react";
 
 type FormDataType = {
-  phone: String;
+  email: string;
 };
 
 const FormDataDefaultValues: FormDataType = {
-  phone: "",
+  email: "",
 };
 
 const ForgetPassword = () => {
   const [formData, setFormData] = useState<FormDataType>(FormDataDefaultValues);
   const [errors, setErrors] = useState<FormDataType | {}>(formData);
+  const [spinner, setSpinner] = useState<boolean>(false);
+
+  const GetPasswordResetLink = async () => {
+    setSpinner(true);
+    try {
+      const verifyResponse = await axios.post(
+        `${process.env.BACKEND_URL}/admin/forget-password`,
+        {
+          email: formData.email,
+          clientUrl: window.location.href.replace("forget", "reset"),
+        }
+      );
+      if (verifyResponse.status === 201) {
+        toast({
+          title: "Password Recover",
+          description: "Check you email inbox to get password reset link!",
+        });
+        setSpinner(false);
+      }
+    } catch (error: any) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: error?.response?.data?.message,
+      });
+      setSpinner(false);
+    }
+  };
 
   const handleOnChange = (
-    e:
-      | ChangeEvent<HTMLInputElement>
-      | { target: { name: String; value: String } }
+    e: ChangeEvent<HTMLInputElement> // Only handle input changes
   ) => {
     const { name, value } = e.target;
-    setFormData({ ...formData, [`${name}`]: value });
+    setFormData({ ...formData, [name]: value });
   };
+
   const handleOnClick = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
@@ -33,7 +62,7 @@ const ForgetPassword = () => {
 
     if (Object.keys(catchedErrors).length === 0) {
       setErrors({});
-      console.log(formData);
+      GetPasswordResetLink();
     } else {
       setErrors(catchedErrors);
     }
@@ -41,12 +70,19 @@ const ForgetPassword = () => {
 
   const validation = (data: FormDataType) => {
     let obj: any = {};
-    if (!data.phone.trim()) {
-      obj.phone = "Phone number is required!";
-    } else if (data.phone.length < 7) {
-      obj.phone = "Phone number is invalid!";
+    if (!data.email.trim()) {
+      obj.email = "Email is required!";
+    } else if (!isValidEmail(data.email)) {
+      obj.email = "Invalid email address!";
     }
     return obj;
+  };
+
+  const isValidEmail = (email: string) => {
+    // You can implement a regex-based email validation function here
+    // For example, you can use a regex like /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    // to validate email addresses.
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
   };
 
   return (
@@ -55,12 +91,22 @@ const ForgetPassword = () => {
         <H3 className="text-center" text="Recover your account!" />
         <form className="grid grid-col-1 gap-4 my-10">
           <div className="grid grid-cols-1 gap-2">
-            <PhoneNumberInput onChange={handleOnChange} />
-            <ErrorMessage errors={errors} name="phone" />
+            <label>Email</label>
+            <input
+              name="email"
+              onChange={handleOnChange}
+              type="email"
+              className="border border-lighter-400 hover:border-[hsl(var(--primary-400))] p-2 rounded-lg focus:outline-none"
+            />
+            <ErrorMessage errors={errors} name="email" />
           </div>
-          <Button className="mt-5 btn-admin" onClick={handleOnClick}>
-            Get verification code
-          </Button>
+          {spinner ? (
+            <ButtonLoading className="mt-5" />
+          ) : (
+            <Button className="mt-5" onClick={handleOnClick}>
+              Get verification code
+            </Button>
+          )}
         </form>
       </div>
     </div>
