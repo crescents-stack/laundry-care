519fe05e72270dc3077e81e3793dac548a8b0a6b
musiur <musiur.opu@gmail.com>
2023-10-20 17:53:21 +0600

    [Feature] User: Schedule Update #146

--- src/app/[lang]/user/dashboard/schedules/page.tsx
+++ src/app/[lang]/user/dashboard/schedules/page.tsx
@@ -1,6 +1,26 @@
 "use client";
 
+import DatePicker from "@/components/custom/datepicker";
+import TimePicker from "@/components/custom/timepicker";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import {
   Table,
   TableBody,
@@ -27,7 +47,26 @@ const headers = [
 ];
 
 export default function Schedules() {
-  const [schedules, setSchedules] = useState([]);
+  const [schedules, setSchedules] = useState<any>([]);
+  const [formData, setFormData] = useState<any>(null);
+  const [errors, setErrors] = useState({});
+
+  const handleOnChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleOnSubmit = (e: any) => {
+    e.preventDefault();
+    const validationErrors = validation(formData);
+    if (Object.keys(validationErrors).length === 0) {
+      console.log(formData);
+    }
+    setErrors(validationErrors);
+  };
+  const validation = (data: any) => {
+    let obj: any = {};
+    return obj;
+  };
   const FetchScheduleAPI = async () => {
     try {
       const token = localStorage.getItem("token");
@@ -40,7 +79,7 @@ export default function Schedules() {
         }
       );
       console.log(response);
-      if(response.status === 200){
+      if (response.status === 200) {
         setSchedules(response.data.schedules);
       }
     } catch (error) {
@@ -50,6 +89,27 @@ export default function Schedules() {
   useEffect(() => {
     FetchScheduleAPI();
   }, []);
+
+  const UpdateProgress = async (data: any) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        `${process.env.BACKEND_URL}/schedules`,
+        { data },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log(response);
+      if (response.status === 200) {
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-5 h-full">
       <h3>Schedules</h3>
@@ -73,89 +133,169 @@ export default function Schedules() {
             </TableRow>
           </TableHeader>
 
-          {
-            schedules.length ? <TableBody>
-            {[...schedules].reverse().map((schedule: any) => (
-              <TableRow key={schedule._id}>
-                {/* <TableCell className="min-w-[100px]">{schedule._id}</TableCell> */}
-                <TableCell className="min-w-[180px]">
-                  {schedule.services.map((service: any) => {
-                    return <div key={service._id}>{service.service}</div>;
-                  })}
-                </TableCell>
-                <TableCell className="min-w-[250px]">
-                  <div className="flex flex-col">
-                    <span className="font-semibold">{schedule.shop.name}</span>
-                    <span className="flex items-center gap-1">
-                      <Phone className="w-3 h-3" /> {schedule.shop.phone}
-                    </span>
-                    <span>
-                      <MapPin className="w-3 h-3 mr-1 inline-block" />{" "}
-                      {schedule.shop.address}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="min-w-[100px]">
-                  <div className="flex flex-col">
-                    <span className="font-semibold">{schedule.rider.name}</span>
-                    <span className="flex items-center gap-1">
-                      <Phone className="w-3 h-3" /> {schedule.rider.phone}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <span className="mr-1">BDT {schedule.payment.amount}</span>
-                  <Badge className="pt-[4px]">{schedule.payment.status}</Badge>
-                </TableCell>
-                <TableCell className="min-w-[100px]">
-                  <div className="flex flex-col">
-                    <span className="flex items-center gap-1">
-                      <Calendar className="w-3 h-3" />
-                      {schedule.collect.date}
-                    </span>
-                    <span className="flex items-center gap-1">
-                      <Clock className="w-3 h-3" />
-                      {schedule.collect.time}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="min-w-[100px]">
-                  <div className="flex flex-col">
-                    <span className="flex items-center gap-1">
-                      <Calendar className="w-3 h-3" />
-                      {schedule.deliver.date}
-                    </span>
-                    <span className="flex items-center gap-1">
-                      <Clock className="w-3 h-3" />
-                      {schedule.deliver.time}
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="min-w-[100px]">
-                  <Badge
-                    className={`pt-[4px] ${
-                      schedule.progress === "DONE"
-                        ? "bg-[hsl(var(--primary-600))] hover:bg-[hsl(var(--primary-700))]"
-                        : "bg-orange-400 hover:bg-orange-500"
-                    }`}
-                  >
-                    {schedule.progress}
-                  </Badge>
-                </TableCell>
-                <TableCell className="min-w-[100px] flex flex-col justify-center gap-2">
-                  <div className="flex gap-2 hover:text-[hsl(var(--primary-600))] group cursor-pointer">
-                    <Edit className="w-4 h-4 group-hover:stroke-[hsl(var(--primary-600))]" />
-                    Progress
-                  </div>
-                  <div className="flex gap-2 text-red-500 cursor-pointer">
-                    <Trash className="w-4 h-4 stroke-red-500" />
-                    Service
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody> : null
-          }
+          {schedules.length ? (
+            <TableBody>
+              {[...schedules].reverse().map((schedule: any) => (
+                <TableRow key={schedule._id}>
+                  <TableCell className="min-w-[180px]">
+                    {schedule.services.map((service: any) => {
+                      return <div key={service._id}>{service.service}</div>;
+                    })}
+                  </TableCell>
+                  <TableCell className="min-w-[250px]">
+                    <div className="flex flex-col">
+                      <span className="font-semibold">
+                        {schedule.shop.name}
+                      </span>
+                      <span className="flex items-center gap-1">
+                        <Phone className="w-3 h-3" /> {schedule.shop.phone}
+                      </span>
+                      <span>
+                        <MapPin className="w-3 h-3 mr-1 inline-block" />{" "}
+                        {schedule.shop.address}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="min-w-[100px]">
+                    <div className="flex flex-col">
+                      <span className="font-semibold">
+                        {schedule.rider.name}
+                      </span>
+                      <span className="flex items-center gap-1">
+                        <Phone className="w-3 h-3" /> {schedule.rider.phone}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell>
+                    <span className="mr-1">BDT {schedule.payment.amount}</span>
+                    <Badge className="pt-[4px]">
+                      {schedule.payment.status}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="min-w-[100px]">
+                    <div className="flex flex-col">
+                      <span className="flex items-center gap-1">
+                        <Calendar className="w-3 h-3" />
+                        {schedule.collect.date}
+                      </span>
+                      <span className="flex items-center gap-1">
+                        <Clock className="w-3 h-3" />
+                        {schedule.collect.time}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="min-w-[100px]">
+                    <div className="flex flex-col">
+                      <span className="flex items-center gap-1">
+                        <Calendar className="w-3 h-3" />
+                        {schedule.deliver.date}
+                      </span>
+                      <span className="flex items-center gap-1">
+                        <Clock className="w-3 h-3" />
+                        {schedule.deliver.time}
+                      </span>
+                    </div>
+                  </TableCell>
+                  <TableCell className="min-w-[100px]">
+                    <Badge
+                      className={`pt-[4px] ${
+                        schedule.progress === "DONE"
+                          ? "bg-[hsl(var(--primary-600))] hover:bg-[hsl(var(--primary-700))]"
+                          : schedule.progress === "PROCESSING"
+                          ? "bg-green-600 hover:bg-green-700"
+                          : "bg-orange-400 hover:bg-orange-500"
+                      }`}
+                    >
+                      {schedule.progress}
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="min-w-[100px] flex flex-col justify-center gap-2">
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <div className="flex gap-2 hover:text-[hsl(var(--primary-600))] group cursor-pointer">
+                          <Edit className="w-4 h-4 group-hover:stroke-[hsl(var(--primary-600))]" />
+                          Progress
+                        </div>
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Edit progress</DialogTitle>
+                          <DialogDescription>
+                            Make changes to serivce progress by selecting
+                            status.
+                          </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid gap-4 py-4">
+                          <div className="grid grid-cols-1 gap-2">
+                            <label className="font-semibold">
+                              Collection Date
+                            </label>
+                            <DatePicker
+                              setter={handleOnChange}
+                              name="collectionDate"
+                            />
+                          </div>
+                          <div className="grid grid-cols-1 gap-2">
+                            <label className="font-semibold">
+                              Collection Time
+                            </label>
+                            <TimePicker
+                              field={{
+                                value: null,
+                                onChange: (e: any) => {
+                                  handleOnChange({
+                                    target: {
+                                      name: "collectionTime",
+                                      value: e,
+                                    },
+                                  });
+                                },
+                              }}
+                            />
+                          </div>
+                          <div className="grid grid-cols-1 gap-2">
+                            <label className="font-semibold">
+                              Delivery Date
+                            </label>
+                            <DatePicker
+                              setter={handleOnChange}
+                              name="deliveryDate"
+                            />
+                          </div>
+
+                          <div className="grid grid-cols-1 gap-2">
+                            <label className="font-semibold">
+                              Delivery Time
+                            </label>
+                            <TimePicker
+                              field={{
+                                value: null,
+                                onChange: (e: any) => {
+                                  handleOnChange({
+                                    target: {
+                                      name: "deliveryTime",
+                                      value: e,
+                                    },
+                                  });
+                                },
+                              }}
+                            />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <Button onClick={handleOnSubmit}>Save changes</Button>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                    <div className="flex gap-2 text-red-500 cursor-pointer">
+                      <Trash className="w-4 h-4 stroke-red-500" />
+                      Service
+                    </div>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          ) : null}
         </Table>
       </div>
     </div>
